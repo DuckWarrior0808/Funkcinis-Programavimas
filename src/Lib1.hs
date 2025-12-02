78daf2be4e5625020f8542e3bb00def8781b9749
Deividas <deividas.matonis@gmail.com>
2025-12-02 20:06:22 +0000

    Labvone

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,16 +1,27 @@
 module Lib1
-    ( examples, Command(..), Dumpable(..)
-    ) where
+  ( examples, Command(..), Dumpable(..)
+  ) where
 
 data Dumpable = Examples
   deriving Show
 
--- This is a "root" ADT representing your grammar,
--- Please expand this ADT as needed
-data Command = Dump Dumpable
+data Command
+  = CreatePhone String String String String
+  | AddPart String (String, String)
+  | AddSubpart String (String, String, [String])
+  | SetCost (String, String, [String]) Double
+  | ListParts (String, String)
+  | CalculateCost (Either (String, String) (String, String, [String]))
+  | Dump Dumpable
   deriving Show
 
 examples :: [Command]
-examples = [
-    Dump Examples
-    ]
+examples =
+  [ CreatePhone "LithuPhone" "Wi-fi 7" "German" "Pro Ultra"
+  , AddPart "Main Board" ("LithuPhone", "LT-10")
+  , AddSubpart "Power IC" ("LithuPhone", "LT-10", ["Main Board"])
+  , SetCost ("LithuPhone", "LT-10", ["Main Board", "Power IC"]) 8.25
+  , ListParts ("LithuPhone", "LT-10")
+  , CalculateCost (Left ("LithuPhone", "LT-10"))
+  , Dump Examples
+  ]
